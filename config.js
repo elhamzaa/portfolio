e5e0452b5f855d1c9caaf003a5401059270769a2
Hamza <h.hassan5971@gmail.com>
2026-01-24 22:41:44 +0200

    New update

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const config = {
     profile: {
         name: "Hamza Hassan",
         role: "AI Engineer",
-        bio: "meow",
+        bio: "بازوكااااااااااااااااااااا",
         cvLink: "#", // Add your CV link here (e.g. /resume.pdf)
         avatar: "https://i.ibb.co/C3fNgSSk/Beauty-Plus-OBJECT-REMOVER-1769110307952-jpg-1.jpg", // Placeholder image
         about: "I am a passionate developer with a strong foundation in AI and web technologies. My journey began with a curiosity for how things work, leading me to explore the depths of machine learning and full-stack development. I love building tools that solve real-world problems and creating digital experiences that leave a lasting impact."
